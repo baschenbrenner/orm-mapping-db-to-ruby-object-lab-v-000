1a7ebf15729ceb58bdf4c439046ce0c2e5cf07db
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-26 14:09:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,4 +62,11 @@ class Student
       SQL
     DB[:conn].execute(sql)
   end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
+      SQL
+    DB[:conn].execute(sql)
+  end
 end
