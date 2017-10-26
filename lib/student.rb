a9521b696b797bcc2c4caff54e01838c78198233
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-26 14:09:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def count_all_students_in_grade_9
+  def self.count_all_students_in_grade_9
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 9
       SQL
