b47ba7151fa3c25a02bd83be6ede03a4fb3f39a2
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-26 13:52:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,17 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def initialize(id,name, grade)
+  end
+  
   def self.new_from_db(row)
-    
+    sql = <<-SQL 
+      SELECT * FROM students
+      SQL
+
+      DB[:conn].execute(sql)
+
+  
   end
 
   def self.all
@@ -14,16 +23,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
