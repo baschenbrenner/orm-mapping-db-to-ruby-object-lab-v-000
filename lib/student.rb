329a545880403cfaf78ba6756abf8f3f28d81dc0
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-10-26 14:17:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,7 +82,7 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = 10 ORDER BY name LIMIT 1
+      SELECT * FROM students WHERE grade = 10 LIMIT 1
       SQL
     DB[:conn].execute(sql)[0]
     binding.pry
